c1a383fa7afddd9dcb9ff9911699309ad1ce6bc9
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-15 11:21:01 +0100

    fix: change allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
