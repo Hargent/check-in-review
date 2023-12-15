f44f55dc686829e0b1c9fb20ca4bc376df94b296
sophie <soffiyahabass1@gmail.com>
2023-12-15 18:33:30 +0100

    first change

--- src/ui/header/index.tsx
+++ src/ui/header/index.tsx
@@ -13,20 +13,21 @@ export default function Header() {
   const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
   return (
     <Modal>
-      <div className=" flex items-center justify-between w-full bg-green-500 py-6 px-5">
+      <div className=" flex items-center justify-between w-full bg-white-500 py-6 px-5  bg-white">
         <div className="">
           <img src={AppLogo} alt="crea8genius" />
         </div>
-        <div className=" flex items-center justify-end space-x-6">
+        <div className=" flex items-center justify-between space-x-6 ">
           {!isLoggedIn && (
             <div className=" flex items-center justify-start space-x-4">
-              <span>welcome</span>
-              <span>Hargent</span>
+              <span className="text-xxl hover:text-black duration-0 text-blue-400 mr-10
+              ">Welcome</span>
+              <span  className="text-xxl hover:text-black duration-0 text-blue-400 mr-20">Hargent</span>
             </div>
           )}
           <Modal.Open id={!isLoggedIn ? ModalId.Login : ModalId.Logout}>
             <div className=" flex items-center justify-between space-x-4">
-              {!isLoggedIn ? <span>login</span> : <span>logout</span>}
+              {!isLoggedIn ? <span  className="text-xxl text-white mr-20 bg-blue-500 border-red-300 px-5 py-2 rounded-md hover:text-blue duration-0 hover:bg-blue-500">Login</span> : <span>logout</span>}
               <IconButtonWrapper extendedClassNames="">
                 <Icons.IconDoor isLoggedIn={isLoggedIn} />
               </IconButtonWrapper>
