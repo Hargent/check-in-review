8006e16579a503e899a65ccd490b84159926b837
Hargent <yemisi3385@gmail.com>
2023-12-16 23:11:28 +0100

    fix: fix deploy errors

--- src/components/error-fallback/error-fallback.tsx
+++ src/components/error-fallback/error-fallback.tsx
@@ -1,43 +1,12 @@
-import styled from "styled-components";
-
-const StyledErrorFallback = styled.main`
-  height: 100vh;
-  background-color: var(--color-grey-50);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 4.8rem;
-`;
-
-const Box = styled.div`
-  /* Box */
-  background-color: var(--color-grey-0);
-  border: 1px solid var(--color-grey-100);
-  border-radius: var(--border-radius-md);
-
-  padding: 4.8rem;
-  flex: 0 1 96rem;
-  text-align: center;
-
-  & h1 {
-    margin-bottom: 1.6rem;
-  }
-
-  & p {
-    font-family: "Sono";
-    margin-bottom: 3.2rem;
-    color: var(--color-grey-500);
-  }
-`;
 type ErrorProps = {
   children: React.ReactElement;
 };
 
 const Error: React.FC<ErrorProps> = ({ children }) => {
   return (
-    <StyledErrorFallback>
-      <Box>{children}</Box>
-    </StyledErrorFallback>
+    <div>
+      <div>{children}</div>
+    </div>
   );
 };
 // export { Box };
