f176f368464f06326dde27539733cd2d41cf4dcf
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-17 12:59:35 +0100

    feat: add super user creation, refactor to models.py for reviews

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from drf_spectacular.views import (
 SpectacularAPIView,
 SpectacularRedocView, 
@@ -10,11 +12,14 @@ SpectacularSwaggerView
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("dj_rest_auth.urls")),
-    path("api/registration/", 
-        include("dj_rest_auth.registration.urls")),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path("management/", admin.site.urls),
+
+     path("api/registeration/", RegisterView.as_view(), name="rest_register"),
+    path("api/login/", LoginView.as_view(), name="rest_login"),
+    path("api/logout/", LogoutView.as_view(), name="rest_logout"),
+    path("api/user/", UserDetailsView.as_view(), name="rest_user_details"),
+
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/redoc/", SpectacularRedocView.as_view(
         url_name="schema"), name="redoc",),
--- reviews/models.py
+++ reviews/models.py
@@ -8,46 +8,46 @@ class Feedback(models.Model):
     "design": "Design",
     "deploy": "Deploy"
     }
-    
+
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="feedback_entries")
     department = models.CharField(max_length=100, choices=DEPARTMENTS)
     week_ending = models.DateField()
-    task_execution_completed = models.BooleanField()
+    task_execution_completed = models.TextField()
     task_execution_challenges = models.TextField()
     task_execution_examples = models.TextField()
 
-    creativity_demonstrated = models.BooleanField()
+    creativity_demonstrated = models.TextField()
     creativity_approach = models.TextField()
     creativity_instances = models.TextField()
 
-    communication_effectiveness = models.CharField(max_length=255)
+    communication_effectiveness = models.TextField()
     communication_clarity = models.TextField()
-    communication_participation = models.BooleanField()
+    communication_participation = models.TextField()
 
-    collaboration_willingness = models.BooleanField()
+    collaboration_willingness = models.TextField()
     collaboration_examples = models.TextField()
-    collaboration_contribution = models.BooleanField()
+    collaboration_contribution = models.TextField()
 
-    initiative_proactive = models.BooleanField()
+    initiative_proactive = models.TextField()
     initiative_instances = models.TextField()
     initiative_areas = models.TextField()
 
-    adaptability_well = models.BooleanField()
+    adaptability_well = models.TextField()
     adaptability_challenges = models.TextField()
     adaptability_examples = models.TextField()
 
-    technical_skills_proficiency = models.CharField(max_length=255)
+    technical_skills_proficiency = models.TextField()
     technical_skills_improvement = models.TextField()
-    technical_skills_assistance = models.BooleanField()
+    technical_skills_assistance = models.TextField()
 
     problem_solving_examples = models.TextField()
-    problem_solving_support = models.BooleanField()
+    problem_solving_support = models.TextField()
 
-    time_management_efficiency = models.CharField(max_length=255)
+    time_management_efficiency = models.TextField()
     time_management_challenges = models.TextField()
     time_management_prioritization = models.TextField()
 
-    professionalism_punctuality = models.BooleanField()
+    professionalism_punctuality = models.TextField()
     professionalism_concerns = models.TextField()
     professionalism_examples = models.TextField()
 
