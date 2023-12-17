8b5a364653287d2b98bc5110696584ee84a921b6
jaypee15 <okoyejohnpaul15@gmail.com>
2023-12-17 15:55:22 +0100

    refactor: read database from env file

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import dj_database_url
 from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,13 +89,18 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+print(os.getenv("password"))
+print(os.getenv("DATABASE_URL"))
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600, conn_health_checks=True),
 }
-
+print (DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
